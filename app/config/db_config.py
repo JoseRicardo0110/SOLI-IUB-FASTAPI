26868ec85258e495ef74e26539fbefe600b9f1f8
JoseRicardo0110 <165341012+JoseRicardo0110@users.noreply.github.com>
2024-07-11 01:09:49 -0500

    o

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,12 +1,16 @@
 import mysql.connector
-from dotenv import load_dotenv
-import os
+""" from dotenv import load_dotenv
+import os """
 
-load_dotenv()
-Host = os.getenv('MYSQ_HOST', 'localhost')
+""" load_dotenv() """
+Host = 'bj46jv7kpkrxwtgl7d7r-mysql.services.clever-cloud.com'
+User = 'ujas9020kpamfmdk'
+Password = 'XN4YSWP1qBoVNuRmMam1'
+Database = 'bj46jv7kpkrxwtgl7d7r'
+""" Host = os.getenv('MYSQ_HOST', '')
 User = os.getenv('MYSQ_USER', 'root')
 Password = os.getenv('MYSQ_PASSWORD', "")
-Database = os.getenv('MYSQ_DB', "bdjosemaria")
+Database = os.getenv('MYSQ_DB', "") """
 
 # ? Conneccion a la base de datos en la nube clever-cloud
 def get_db_connection():
