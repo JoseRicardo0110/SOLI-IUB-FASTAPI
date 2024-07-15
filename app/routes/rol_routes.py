9712a275251177591822cb20101e781f62b92c53
JoseRicardo0110 <165341012+JoseRicardo0110@users.noreply.github.com>
2024-07-14 22:58:52 -0500

    w

--- app/routes/rol_routes.py
+++ app/routes/rol_routes.py
@@ -3,7 +3,6 @@ from controllers.rol_controller import *
 from models.rol_model import Rol
 from typing import List
 from fastapi.encoders import jsonable_encoder
-from typing import List
 import mysql.connector
 
 
@@ -11,14 +10,42 @@ router = APIRouter()
 
 nuevo_rol = RolController()
 
+@router.get("/get_all_modulos", response_model=List[dict])
+def get_all_modulos():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, modulo FROM modulos")
+        result = cursor.fetchall()
+        payload = []
+
+        for mod in result:
+            content = {
+                'id': mod[0],
+                'modulo': mod[1]
+            }
+            payload.append(content)
+
+        json_data = jsonable_encoder(payload)            
+        if result:
+            return json_data
+        else:
+            raise HTTPException(status_code=404, detail="No modules found")  
+                
+    except mysql.connector.Error as err:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(err))
+    finally:
+        conn.close()
+
 @router.get("/get_Roles/")
 async def get_Roles():
     rpta = nuevo_rol.get_Roles()
     return rpta
-@router.get("/get_all_modulos")
+"""@router.get("/get_all_modulos")
 async def get_all_modulos():
     rpta = nuevo_rol.get_all_modulos()
-    return rpta
+    return rpta"""
 @router.get("/get_ModulosxRoles")
 async def get_ModulosxRoles():
     rpta = nuevo_rol.get_ModulosxRoles()
